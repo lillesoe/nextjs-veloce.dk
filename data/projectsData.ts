b1a4ad47c2511bc8a27ee8ea7780ef94ac43327f
Jesper <jesper@Jespers-MBP.local>
2022-09-09 22:30:13 +0200

    Initial blot post made

---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'Photos',
+    description: `Photos - lots of photos. For documentation, learning, and fun.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+]
+
+export default projectsData
