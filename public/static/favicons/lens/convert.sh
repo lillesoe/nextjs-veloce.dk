3a40784d6d45d163c7da1d9111da7445baf14257
Jesper <lillesoe@gmail.com>
2022-09-18 13:10:35 +0200

    Transparent favicon

---
+++ public/static/favicons/lens/convert.sh
@@ -0,0 +1,7 @@
+convert lens-tranparent.png -resize 96x96 ../android-chrome-96x96.png
+convert lens-tranparent.png -resize 76x76 ../apple-touch-icon.png  
+convert lens-tranparent.png -resize 16x16 ../favicon-16x16.png 
+convert lens-tranparent.png -resize 32x32 ../favicon-32x32.png
+convert lens-tranparent.png -resize 48x48 ../favicon.png  
+convert lens-tranparent.png -resize 229x229 ../mstile-150x150.png
+
